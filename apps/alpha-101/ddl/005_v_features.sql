027a8ae72da106bea9acd70a04f9c7ea94c93b34
Gang Tao <gang@timeplus.io>
2026-05-20 11:26:34 -0700

    alpha-101: features view (returns, 20-bar sigma, signed_power)

---
+++ apps/alpha-101/ddl/005_v_features.sql
@@ -0,0 +1,29 @@
+CREATE OR REPLACE VIEW {{ .DB }}.v_features AS
+SELECT
+  time, stock_id, close, returns, sigma_ret_20,
+  cond,
+  sign(cond) * cond * cond AS signed_power
+FROM (
+  SELECT
+    time, stock_id, close, returns, sigma_ret_20,
+    if(returns < 0, sigma_ret_20, close) AS cond
+  FROM (
+    SELECT
+      time, stock_id, close,
+      (array_element(close21, 1) - array_element(close21, 2)) / nullif(array_element(close21, 2), 0) AS returns,
+      array_reduce(
+        'stddev_pop',
+        array_map(
+          i -> (array_element(close21, i) - array_element(close21, i + 1)) / nullif(array_element(close21, i + 1), 0),
+          range(1, 21)
+        )
+      ) AS sigma_ret_20
+    FROM (
+      SELECT
+        time, stock_id, close,
+        lags(close, 0, 20) AS close21
+      FROM {{ .DB }}.v_bars
+      PARTITION BY stock_id
+    )
+  )
+)
